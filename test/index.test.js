45ce03188af543598a7dd47d67149879fa7701b3
tcdian <tcdiank@gmail.com>
2021-05-28 19:15:38 +0800

    ✅ optimize tests

--- test/index.test.js
+++ test/index.test.js
@@ -3,7 +3,7 @@ const asy = require('../source');
 describe('with one promise yield', () => {
     test('should work', async () => {
         expect.assertions(1);
-        let asyTask = asy(function* (value) {
+        const asyTask = asy(function* (value) {
             return yield Promise.resolve(value);
         });
         await expect(asyTask('success')).resolves.toBe('success');
@@ -13,7 +13,7 @@ describe('with one promise yield', () => {
 describe('with several promise yields', () => {
     test('should work', async () => {
         expect.assertions(1);
-        let asyTask = asy(function* () {
+        const asyTask = asy(function* () {
             var a = yield Promise.resolve(1);
             var b = yield Promise.resolve(2);
             var c = yield Promise.resolve(3);
@@ -27,14 +27,14 @@ describe('with several promise yields', () => {
 describe('when a promise is rejected', () => {
     test('should throw', async () => {
         expect.assertions(1);
-        let asyTask = asy(function* () {
+        const asyTask = asy(function* () {
             return yield Promise.reject(new Error('fail!!!'));
         });
         await expect(asyTask()).rejects.toEqual(new Error('fail!!!'));
     });
     test('should resume', async () => {
         expect.assertions(1);
-        let asyTask = asy(function* () {
+        const asyTask = asy(function* () {
             try {
                 return yield Promise.reject(new Error('fail!!!'));
             } catch (err) {
